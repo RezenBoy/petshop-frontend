d4965270b6892a0c0199e56787dafa42b42e8d2c
RezenBoy <officalproton@gmail.com>
2026-05-08 12:24:44 +0530

    api url fix

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,7 +1,7 @@
 // src/libs/api.js
 import axios from "axios";
 
-const API = process.env.REACT_APP_API_URL || "https://petshop-backend-44re.onrender.com";
+const API = "https://petshop-backend-44re.onrender.com";
 
 const api = axios.create({
   baseURL: API + "/api",
